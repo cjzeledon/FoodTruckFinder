d69d1a50940715ff69b93114e43ddd2a75d86c7f
Claudia Zeledon <cjzeledon@gmail.com>
2017-10-20 14:29:43 -0400

    removing merge symbols from the interface

--- src/main/java/com/theironyard/charlotte/FoodTruckFinder/repositories/UserRepository.java
+++ src/main/java/com/theironyard/charlotte/FoodTruckFinder/repositories/UserRepository.java
@@ -3,12 +3,5 @@ package com.theironyard.charlotte.FoodTruckFinder.repositories;
 import com.theironyard.charlotte.FoodTruckFinder.models.database.User;
 import org.springframework.data.repository.CrudRepository;
 
-<<<<<<< HEAD
-public interface UserRepository
-        extends CrudRepository<User, Integer>{
-
-
-=======
 public interface UserRepository extends CrudRepository<User, Integer>{
->>>>>>> ed9c7c4a44c45ceb917b5688e8962e82eed2ec59
 }
